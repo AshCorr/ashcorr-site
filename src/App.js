72f4eb6a625a00efc3470c746bcea97b87eb6ec2
AshCorr <ashcorr20@gmail.com>
2021-09-16 23:20:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <p>I should put something here! ... someday :)</p>
     </div>
   );
 }
